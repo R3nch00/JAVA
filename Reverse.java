6f90abdf8abfc4c9d53a6f5323253742a35e14b0
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-03-21 23:08:35 +0600

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,8 @@
+package com.company;
+public class Main {
+    public static void main(String[] args){
+        StringBuffer sbr=new StringBuffer("Mango");
+        sbr.reverse();
+        System.out.println(sbr); // ognaM
+    }
+}
