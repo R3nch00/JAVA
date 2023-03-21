5dc9821b6456ca1d62dd5f77187ce27d2767d0d6
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-03-21 23:09:03 +0600

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,9 @@
+package com.company;
+    class Main
+    {
+        public static void main (String[] args)
+        {
+            String s= "GeeksforGeeks";
+            System.out.println("String length = " + s.length());
+	}
+     }
