1856bd8249db1cdfd5c8b4a067334276e2494b2d
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-03-23 00:47:56 +0600

    Add files via upload

---
+++ Replace.java
@@ -0,0 +1,11 @@
+package com.company;
+    class Main
+    {
+        public static void main (String[] args)
+	{
+    String str1 = "feeksforfeeks";
+        System.out.println("Original String :" + str1);
+    String str2 = "feeksforfeeks".replace('f' ,'g') ;
+        System.out.println("Replaced f with g -> :" + str2);
+	}
+    }
